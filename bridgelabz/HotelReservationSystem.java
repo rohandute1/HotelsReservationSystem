ec95469fc1503505d8ba067b9f088b400a275805
Rohan <rohandute1@gmail.com>
2023-11-30 20:38:45 +0530

    Added:- successfully added hotels to the hotel reservation system

--- bridgelabz/HotelReservationSystem.java
+++ bridgelabz/HotelReservationSystem.java
@@ -1,7 +1,19 @@
 package com.bridgelabz;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class HotelReservationSystem {
+    static List<Hotel> hotels=new ArrayList<>();
     public static void main(String[] args) {
         System.out.println("Welcome to hotel reservation system");
+
+        Hotel Lakewood = new Hotel("Lakewood", 110);
+        Hotel Bridgewood = new Hotel("Bridgewood", 160);
+        Hotel Ridgewood = new Hotel("Ridgewood", 220);
+        hotels.add(Lakewood);
+        hotels.add(Bridgewood);
+        hotels.add(Ridgewood);
+        System.out.println(hotels);
     }
 }
